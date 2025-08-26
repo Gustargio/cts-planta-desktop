2136c1a267dd559056148f5b5ead635abb8fce0b
Gustargio <inviertoen@gmail.com>
2025-08-26 10:22:23 -0300

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,14 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('api', {
-  // Notificaciones
-  notify: (title, body) => ipcRenderer.invoke('notify', { title, body }),
-  notifyAckable: (title, body, mixerId, status) =>
-    ipcRenderer.invoke('notifyAckable', { title, body, mixerId, status }),
-
-  // Click en botón “Dar OK” desde la notificación
-  onAckClick: (cb) => ipcRenderer.on('ack-notify-click', (_e, data) => cb(data)),
-
-  // Menú
-  onMenu: (channel, cb) => ipcRenderer.on(channel, (_e, data) => cb(data))
+contextBridge.exposeInMainWorld('planta', {
+  notifyAck: async (title, body) => {
+    try {
+      await ipcRenderer.invoke('show-ack-noti', { title, body });
+    } catch (e) {
+      // fallback si algo falla
+      new Notification(title || 'Cambio', { body: body || '' });
+    }
+  }
 });
